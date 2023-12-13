5d3cbecacbbf58d2b88817db619e6acb4911211a
Nemo Grippa <nemo.grippa@gmail.com>
2023-12-13 21:48:42 +0100

    simplify day 13 a lot

--- days/13/13.ts
+++ days/13/13.ts
@@ -3,60 +3,27 @@ import { Solve, Star } from "../../utils/types";
 import { flipField } from "../../utils/processing";
 import { stringsHaveExactlyOneDiff } from "../../utils/string";
 
-type PotentialSymmetry = { i: number; usedSmudge: boolean };
-
-const findPotentialSymmetriesInField =
+const findSymmetries =
   (star: Star) =>
-  (field: string[]): PotentialSymmetry[] =>
-    field.reduce((acc, curr, i) => {
-      const next = field[i + 1];
-      if (!next) return acc;
-      if (curr === next) {
-        acc.push({ i, usedSmudge: false });
-      } else {
-        if (star === 2) {
-          const cmp = stringsHaveExactlyOneDiff(curr, next);
-          if (cmp !== null) {
-            acc.push({ i, usedSmudge: true });
-          }
-        }
+  (field: string[]): number => {
+    const confirmedSymmetries: number[] = [];
+
+    axis: for (let start = 0; start < field.length - 1; start++) {
+      let smudge = false;
+      for (let a = start; a >= 0; a--) {
+        const b = 2 * start - a + 1;
+        if (!field[b]) break;
+        if (field[a] === field[b]) continue;
+        if (star === 1 || smudge) continue axis;
+        const cmp = stringsHaveExactlyOneDiff(field[a], field[b]);
+        if (cmp === null) continue axis;
+        smudge = true;
       }
-      return acc;
-    }, [] as PotentialSymmetry[]);
+      if (star === 1 || smudge) confirmedSymmetries.push(start);
+    }
 
-const confirmSymmetriesInField =
-  (star: Star) =>
-  (field: string[], potentialSymmetries: PotentialSymmetry[]): number => {
-    const confirmedSymmetries = potentialSymmetries.filter(
-      ({ i, usedSmudge }) => {
-        let smudge = usedSmudge;
-        for (let a = i - 1; a >= 0; a--) {
-          const b = 2 * i - a + 1;
-          if (!field[b]) break;
-          if (field[a] !== field[b]) {
-            if (star === 2 && !smudge) {
-              const cmp = stringsHaveExactlyOneDiff(field[a], field[b]);
-              if (cmp === null) {
-                return false;
-              } else {
-                smudge = true;
-              }
-            } else {
-              return false;
-            }
-          }
-        }
-        if (star === 2) return smudge;
-        return true;
-      },
-    );
-    return confirmedSymmetries.reduce((acc, curr) => acc + curr.i + 1, 0);
+    return confirmedSymmetries.reduce((acc, curr) => acc + curr + 1, 0);
   };
-
-const findSymmetries = (star: Star) => (field: string[]) => {
-  const potential = findPotentialSymmetriesInField(star)(field);
-  return confirmSymmetriesInField(star)(field, potential);
-};
 
 export const solve: Solve = (star, input) => {
   const data = loadDataGrouped(star, input, __dirname);
