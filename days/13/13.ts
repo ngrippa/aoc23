ca1aeea2a7981f3839f5fa99c655c8b8903c58d0
Nemo Grippa <nemo.grippa@gmail.com>
2023-12-13 22:26:03 +0100

    simplify findSymmetries

--- days/13/13.ts
+++ days/13/13.ts
@@ -6,32 +6,26 @@ import { stringsHaveExactlyOneDiff } from "../../utils/string";
 const findSymmetries =
   (star: Star) =>
   (field: string[]): number => {
-    const confirmedSymmetries: number[] = [];
-
     axis: for (let start = 0; start < field.length - 1; start++) {
-      let smudge = false;
+      let smudge = star === 1;
       for (let a = start; a >= 0; a--) {
         const b = 2 * start - a + 1;
         if (!field[b]) break;
         if (field[a] === field[b]) continue;
-        if (star === 1 || smudge) continue axis;
-        const cmp = stringsHaveExactlyOneDiff(field[a], field[b]);
-        if (cmp === null) continue axis;
+        if (smudge || stringsHaveExactlyOneDiff(field[a], field[b]) === null)
+          continue axis;
         smudge = true;
       }
-      if (star === 1 || smudge) confirmedSymmetries.push(start);
+      if (smudge) return start + 1;
     }
-
-    return confirmedSymmetries.reduce((acc, curr) => acc + curr + 1, 0);
+    return 0;
   };
 
 export const solve: Solve = (star, input) => {
   const data = loadDataGrouped(star, input, __dirname);
   const findSym = findSymmetries(star);
-  const horizontal = data.reduce((acc, field) => acc + findSym(field), 0);
-  const vertical = data.reduce(
-    (acc, field) => acc + findSym(flipField(field)),
+  return data.reduce(
+    (acc, field) => acc + (findSym(field) * 100 || findSym(flipField(field))),
     0,
   );
-  return horizontal * 100 + vertical;
 };
